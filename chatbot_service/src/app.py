34add9e7a07041b6d0b965af467cba1289bcca79
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2020-04-21 19:03:11 -0300

    app: use chitchat to get responses

--- chatbot_service/src/app.py
+++ chatbot_service/src/app.py
@@ -3,22 +3,37 @@ from flask import request
 from flask_cors import CORS
 
 import json
+from chitchat import send_message, load_model_config
 
 app = Flask(__name__)
 CORS(app)
+model_config = load_model_config()
+histories = {}
+
 
 @app.route('/<talk_session>', methods=['POST'])
 def talk_session(talk_session):
     message = json.loads(request.data.decode('utf-8'))
     text = message['text']
 
+    if talk_session not in histories:
+        histories[talk_session] = {
+            'messages': [],
+            'personality': None,
+        }
+
     print(f'Talk session: {talk_session}')
     print(f'Message received: {text}')
+    print(histories)
 
-    message['text'] += '\n\nHere we can preprocess the text, mapping acronyms,\
-    as well as translating it, for example.'
+    # TODO: Preprocess the text to the chatbot service. (Translate, remove
+    # acronyms, etc
 
-    # TODO: Send the preprocessed text to the chatbot service and get its
-    # response. Then, we must to retranslate the output text the original idiom.
+    response_msg = send_message(histories[talk_session], text, model_config)
+    print(response_msg)
 
-    return json.dumps(message)
+    # TODO: Translate the text back to the original idiom
+
+    return json.dumps({
+        'text': response_msg,
+    })
