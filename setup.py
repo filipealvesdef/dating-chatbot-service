23def16a4280e7aa6cc5e10d41531bfb8399ce76
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2020-04-21 13:27:22 -0300

    setup: add setup.py
    Flask added as requirement

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Dating chatbot API',
+    description='An API that comunicates with a chatbot service',
+    version='0.0.1',
+    packages=[
+        'chatbot_service',
+    ],
+    install_requires=[
+        'flask~=1.1.2',
+    ],
+)
